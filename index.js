fdeab41973d7ab6ca0101a894b7292e47eed61d8
Nicolas Peñaloza <57502001+nicopenaloza@users.noreply.github.com>
2022-03-07 15:09:41 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,52 @@
+const {
+    initializeApp,
+    applicationDefault,
+    cert,
+} = require("firebase-admin/app");
+const {
+    getFirestore,
+    Timestamp,
+    FieldValue,
+} = require("firebase-admin/firestore");
+
+class app {
+
+    db;
+    app;
+
+    init(credentials){
+        this.app = initializeApp(credentials.app),
+        this.db = getFirestore();
+    }
+
+    readyToRun() {
+        return Boolean(this.db && this.app);
+    }
+
+    sendMessage(msg) {
+        if (!this.readyToRun()) {
+            console.error("Firebase not ready to run yet, please initialize first");
+            return;
+        }
+        this.db.collection("messages").add({
+            firebase_id: msg.firebase_id,
+            data: {
+                ...msg.data,
+                datetime: Timestamp.now()
+
+            }
+        });
+    }
+
+    getMessages(user_id, limit = 10) {
+        if (!this.readyToRun()) {
+            console.error("Firebase not ready to run yet, please initialize first");
+            return;
+        }
+        return this.db.collection("messages").where("firebase_id", "==", user_id).orderBy("datetime", "desc").limit(limit).get();
+    }
+
+}
+
+const pedimeloChat = new app();
+export default pedimeloChat;
