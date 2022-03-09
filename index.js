29617fb4ef060b30fdf0ac8ad2dc361609498bcc
Nicolas Peñaloza <57502001+nicopenaloza@users.noreply.github.com>
2022-03-09 11:55:44 -0300

    main update

--- index.js
+++ index.js
@@ -1,49 +1,80 @@
-const {
-    initializeApp,
-} = require("firebase-admin/app");
-const {
-    getFirestore,
-    Timestamp,
-} = require("firebase-admin/firestore");
+const { initializeApp } = require("firebase-admin/app");
+const { getFirestore, Timestamp } = require("firebase-admin/firestore");
 
 class app {
-
     db;
     app;
 
-    init(credentials){
-        this.app = initializeApp(credentials.app),
-        this.db = getFirestore();
+    init(credentials) {
+        (this.app = initializeApp(credentials.app)), (this.db = getFirestore());
     }
 
     readyToRun() {
         return Boolean(this.db && this.app);
     }
 
-    sendMessage(msg) {
-        if (!this.readyToRun()) {
-            console.error("Firebase not ready to run yet, please initialize first");
-            return;
-        }
-        this.db.collection("messages").add({
-            firebase_id: msg.firebase_id,
-            data: {
-                ...msg.data,
-                datetime: Timestamp.now()
+    noInicializadoError() {
+        console.error(
+            "PedimeloChat no fue inicializado todavia, por favor inicializalo primero"
+        );
+    }
 
+    async sendMessage(msg) {
+        if (!this.readyToRun()) {
+            this.noInicializadoError();
+            return false;
+        }
+
+        let ref = this.db.collection("messages").doc(msg.firebase_id);
+        if (!ref) return false;
+
+        const doc = await ref.get();
+        if (doc.exists) {
+            const data = doc.data();
+            if (!data) {
+                console.error(`Checkea personalmente el documento ${msg.firebase_id} porque no cuenta con datos, probablemente convenga borrarlo y volver a intentarlo`);
+                return false;
             }
-        });
-    }
 
-    getMessages(user_id, limit = 10) {
-        if (!this.readyToRun()) {
-            console.error("Firebase not ready to run yet, please initialize first");
-            return;
+            await this.db
+                .collection("messages")
+                .doc(msg.firebase_id)
+                .update({
+                    mensajes: [
+                        ...data.mensajes,
+                        {
+                            event: msg.event,
+                            datetime: Timestamp.now(),
+                            payload: msg.payload,
+                        },
+                    ],
+                });
+            return true;
         }
-        return this.db.collection("messages").where("firebase_id", "==", user_id).orderBy("datetime", "desc").limit(limit).get();
+        await this.db
+            .collection("messages")
+            .doc(msg.firebase_id)
+            .set({
+                mensajes: [
+                    {
+                        event: msg.event,
+                        datetime: Timestamp.now(),
+                        payload: msg.payload,
+                    },
+                ],
+            });
+        return true;
     }
 
+    async getMessages(user_id) {
+        if (!this.readyToRun()) {
+            this.noInicializadoError();
+            return false;
+        }
+        const data = await this.db.collection("messages").doc(user_id).get();
+        return data;
+    }
 }
 
 const pedimeloChat = new app();
-export default pedimeloChat;
+exports.pedimeloChat = pedimeloChat;
